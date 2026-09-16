ef1c0b4d8f431285c15dae12c876727faf25b1cc
yyyumeniku <eivordoudo2021@gmail.com>
2026-09-15 23:06:00 -0400

    mod + a opens the calendar and mod + s the notification center

--- hypr/programs.lua
+++ hypr/programs.lua
@@ -8,6 +8,7 @@ return {
     memory         = "qs ipc call shell memory",
     volume         = "qs ipc call shell volume",
     calendar       = "qs ipc call shell calendar",
+    notifications  = "qs ipc call shell notifications",
     contributions  = "qs ipc call shell github",
     wallpaper      = "/home/gabriel/Documents/Wallpaper/12-Monterey-Dark.jpg",
 }
