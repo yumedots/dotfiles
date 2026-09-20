5450f8d5bd5562e5a74784e4e755e72cf3a3e84f
yyyumeniku <eivordoudo2021@gmail.com>
2026-09-20 00:03:01 -0400

    the bar has more space between its widgets

--- quickshell/config.js
+++ quickshell/config.js
@@ -22,7 +22,7 @@ const fontSize = 12;
 const valueSample = "0123456789.%G";
 const iconSize = 10;
 const trayIconSize = 11;
-const spacing = 12;
+const spacing = 20;
 const workspacePadding = 1;
 const workspaceSpacing = 6;
 
